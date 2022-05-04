88ae2db9aa8d28aa52eef9dff706b230d5a52725
EarlTP <luca.chiavelli.36@gmail.com>
2022-05-04 18:38:59 +0200

    android pre build

--- android/app/appcenter-post-clone.sh
+++ android/app/appcenter-post-clone.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 
-echo "ciao"
+# root
+echo "mi sposto nella root del progetto"
+cd ../..
 
 # aggiornamento librerie
+echo "aggiornamento librerie"
 npm i
 
 # sync
+echo "sync"
 ionic capacitor sync android
+
+# android
+echo "mi sposto in android"
+cd /android/app
--- android/appcenter-post-clone.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# aggiornamento librerie
-npm i
-
-# sync
-ionic capacitor sync android
